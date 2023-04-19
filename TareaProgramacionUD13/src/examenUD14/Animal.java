ecea4125d14ff840af50e6ba0099f7fdc7d99868
MarcosFdS <marcos.fernandez@live.u-tad.com>
2023-04-19 09:32:30 +0200

    commit 1

---
+++ TareaProgramacionUD13/src/examenUD14/Animal.java
@@ -0,0 +1,17 @@
+package examenUD14;
+
+public class Animal {
+	
+	private String nombre;
+	private String genero;
+	private String especie;
+	private int edad;
+	
+	public Animal(String nombre, String genero, String especie, int edad) {
+		this.nombre = nombre;
+		this.genero = genero;
+		this.especie = especie;
+		this.edad = edad;
+	}
+
+}
